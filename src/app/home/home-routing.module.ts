575b8b14ca7f93bdbd03f40afb82da19896503f5
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-01 17:59:14 -0500

    Add component routes to home module, route home as initial path.

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,17 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeTopComponent } from './home-top/home-top.component';
+import { HomeContentComponent } from './home-content/home-content.component';
+
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeTopComponent},
+  {path: 'home', component: HomeContentComponent},
+  {path: '', redirectTo: '/home', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
