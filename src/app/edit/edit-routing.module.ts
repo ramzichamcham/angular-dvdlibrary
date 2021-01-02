c501fea8d0782bc6dfe40a732b33e35f191b0db2
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-02 12:51:23 -0500

    Add EditHomeComponent route to edit-routing.module.ts

--- src/app/edit/edit-routing.module.ts
+++ src/app/edit/edit-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditHomeComponent } from './edit-home/edit-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'edit', component: EditHomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
