290f836bacd5f617d99b94991edda4a1a34a07c0
Ramzi Chamcham <ramzichamcham@gmail.com>
2021-01-06 14:31:41 -0500

    home-content clean up and comments

--- src/app/home/home-content/home-content.component.ts
+++ src/app/home/home-content/home-content.component.ts
@@ -21,47 +21,47 @@ export class HomeContentComponent implements OnInit {
 
   @Input() term;
 
-  constructor(private dvdServ: DvdService, private route: ActivatedRoute, private location: Location) {
-    
-   }
+  constructor(private dvdServ: DvdService, private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit(): void {
     
+    //load all dvds
     this.getDvds();
 
   }
 
-
+    //get dvds using service
   getDvds(): void{
     this.dvdServ.getDvds()
     .subscribe(dvds => this.dvds = dvds);
   }
 
+  //get dvds by category using service
   getByCategory(){
-    console.log(this.category);
-    console.log(this.term);
-
     if(this.validInput()){
       this.errors='';
       this.dvdServ.getByCategory(this.category, this.term)
       .subscribe(dvds => this.dvds = dvds);
-    }else{
-
     }
 
   }
 
+  //delete selected dvd
   delete(id: number){
+    //validate user decision
     if(window.confirm('Are sure you want to delete this Dvd from your collection ?')){
       this.dvdServ.deleteById(id)
       .subscribe(response => console.log(response));
       
-      this.dvds = this.dvds.filter(function(obj){
-        return obj.id !== id;
+      //remove dvd from dvds array
+      this.dvds = this.dvds.filter(function(dvd){
+        return dvd.id !== id;
     });
     }
+
   }
 
+  //User input error handling
   validInput(): boolean{
     if(this.category=="" || this.term==""){
       this.errors='<li class=" alert alert-danger">Both Search Category and Search Term are required</li>';
